76f41ac2e06f587ba218a92ddc03a8a94b62ad6f
nicolas <nicolas.rossat@hotmail.fr>
2022-09-13 12:25:28 +0200

    Ajout page ajout film et fonction ajouter dans firebase

--- testfirebase/lib/main.dart
+++ testfirebase/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'add_movie_page.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -30,7 +31,14 @@ class HomePage extends StatelessWidget {
         title: const Text('Bibliothèque de films'),
         leading: IconButton(
             onPressed: () {
-              print('btn ajouter ok');
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (BuildContext context) {
+                    return const AddPage();
+                  },
+                  fullscreenDialog: true,
+                ),
+              );
             },
             icon: const Icon(Icons.add)),
       ),
